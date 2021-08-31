d585b786e34b5509e49e2c2e6ffa19b31450c961
sakamona <sakamona@cocoto.co.jp>
2021-08-31 18:17:14 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/naomasa/sakamoto/kotlinlog/MainActivity.kt
@@ -0,0 +1,80 @@
+package jp.techacademy.naomasa.sakamoto.kotlinlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("kotlintest", "ログへの出力テスト")
+
+        /*var num = 10
+        Log.d("kotlintest", Integer.toString(num))
+
+        num = 50
+        Log.d("kotlintest", num.toString())
+
+        val num1 = 10 + 5 - 2 * 4 / 2
+        Log.d("kotlintest", "10 + 5 - 2 * 4 / 2 = " + num1)
+
+        val flag1 = true
+        val flag2 = false
+        Log.d("kotlintest", "flag1 && flag2 = " + (flag1 && flag2))
+        Log.d("kotlintest", "flag1 || flag2 = " + (flag1 || flag2))
+
+        val num2 = 10
+        val num3 = 20
+        Log.d("kotlintest", "num2 < num3 = " + (num2 < num3))
+
+        num = 60
+
+        if (num >= 90) {
+            Log.d("kotlintest", "優")
+        } else if (num >= 75){
+            Log.d("kotlintest", "良")
+        } else if (num >= 60) {
+            Log.d("kotlintest", "可")
+        } else {
+            Log.d("kotlintest", "不可")
+        }
+
+        val drink = 1
+
+        when (drink) {
+            0 -> {
+                Log.d("kotlintest", "コーヒーを注文しました")
+            }
+            1 -> Log.d("kotlintest", "紅茶を注文しました")
+            2 -> Log.d("kotlintest", "ミルクを注文しました")
+            else -> Log.d("kotlintest", "オーダーミスです")
+        } */
+
+        for (i in 1 until 6 step 1) {
+            Log.d("kotlintest", "for文の" + i + "回目")
+        }
+
+        for (i in 6 downTo 2 step 2) {
+            Log.d("kotlintest", "for文の" + i + "回目")
+        }
+
+        var num = 1
+
+        while (num < 6) {
+            Log.d("kotlintest", "while文の" + num + "回目")
+            num++
+        }
+
+        val points = arrayOf(10, 6, 15, 23, 17)
+
+        var i = 2
+
+        for (i in points) {
+            Log.d("kotlintest", i.toString())
+        }
+
+
+    }
+}
